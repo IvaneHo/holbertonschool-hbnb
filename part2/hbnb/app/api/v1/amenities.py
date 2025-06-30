d84aca28c329c419de282d14813fae90b3b6b03a
Hamza-coder3011 <hammadi.hamza@hotmail.com>
2025-06-30 11:14:50 +0200

    Update amenities

--- part2/hbnb/app/api/v1/amenities.py
+++ part2/hbnb/app/api/v1/amenities.py
@@ -1,4 +1,4 @@
-from flask_restx import Namespace, Resource
+from flask_restx import Namespace, Resource, fields
 from flask import request
 from app.services.facade import facade
 
@@ -9,13 +9,23 @@ api = Namespace(
     description="Amenity operations"
 )
 
+amenity_model = api.model('Amenity', {
+    'id': fields.String(readonly=True, description='Amenity unique ID'),
+    'name': fields.String(required=True, description='Name of the amenity')
+})
 
 @api.route("/")
 class AmenityListResource(Resource):
+    @api.doc('List_amenities')
+    @api.marshal_list_with(amenity_model)
     def get(self):
         # Récupère et retourne la liste de toutes les amenities
         return facade.get_all_amenities(), 200
-
+    
+    @api.doc('create_amenity')
+    @api.expect(amenity_model)
+    @api.response(201, 'Amenity successfully created')
+    @api.response(400, 'Invalid data')
     def post(self):
         # Crée une nouvelle amenity à partir des données JSON reçues
         try:
@@ -27,7 +37,11 @@ class AmenityListResource(Resource):
 
 
 @api.route("/<string:amenity_id>")
+@api.param('amenity_id', 'Amenity identifier')
 class AmenityResource(Resource):
+    @api.doc('get_amenity')
+    @api.marshal_with(amenity_model)
+    @api.response(404, 'Amenity not found')
     def get(self, amenity_id):
         # Récupère une amenity par son ID
         result = facade.get_amenity(amenity_id)
@@ -35,7 +49,12 @@ class AmenityResource(Resource):
             # Retourne une erreur si l'amenity n'existe pas
             return {"error": "Amenity not found"}, 404
         return result, 200
-
+    
+    @api.doc('update_amenity')
+    @api.expect(amenity_model)
+    @api.response(200, 'Amenity successfully updated')
+    @api.response(400, 'Invalid input')
+    @api.response(404, 'Amenity not found')
     def put(self, amenity_id):
         # Met à jour une amenity existante avec les nouvelles données
         try:
